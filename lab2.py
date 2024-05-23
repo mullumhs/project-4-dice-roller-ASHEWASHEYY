a1d6f32996ff6c02dbb4a5236704c0e5b000e3cd
ASHEWASHEYY <bella.doodyharding@education.nsw.gov.au>
2024-05-24 09:49:28 +1000

    today i didnt lab 1 2 and mefr

--- lab2.py
+++ lab2.py
@@ -9,14 +9,28 @@
 
 # Create a function named say_hello that accepts a person's name as a parameter and prints "Hello" followed by the name.
 
+def say_hello(name):
+    print(f"hi {name}")
+
 
 # Develop a function named triple that takes one number as a parameter and returns the number multiplied by three.
+
+def triple(number):
+    return(number*3)
 
 
 # Write a function called add that takes two numbers as parameters and returns their sum. 
 
+def add(num1, num2):
+    print(num1 + num2)
 
 # Create a function named draw_line that takes one parameter for the length of the line and prints a straight line of that many hyphens.
 
+def draw_line(para):
+    print("-" * para)
 
 # Call your functions, printing out the return result where appropriate
+draw_line(10)
+add(2, 5)
+triple(83)
+say_hello("MMMMMMMMMEOOOOOOOOOWWWWWWWWWWWWWWW")
--- lab3.py
+++ lab3.py
@@ -10,12 +10,26 @@
 # Define a function called get_number(prompt) that returns an integer. 
 # Include a while loop and try/except blocks inside the function to handle non-integer inputs.
 
-
+def get_number():
+    while True:
+        try:
+            num = int(input("enter a numba:"))
+            return num
+        except:
+            print("wrong bozo get gud")
 
 # Use the get_number function to ask for a numerator, then again for a denominator.
 # Divide the numerator by the denominator and print the result, handling any division by zero errors.
-
-
+numerator = get_number()
+denominator = get_number
+while True:
+    try:
+        result = numerator / denominator
+        break
+    except:
+        print("LOSER NAW")
+        denominator = get_number()
+print(result)
 
 # Use the get_number function to ask the user for an index to access an element from a predefined list. 
 # Print out the value from the list, handling the index error if the user selects a non-existent index.
